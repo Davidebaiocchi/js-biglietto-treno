565f72cf0d46ede930a64157cfdd32947a0c0731
davide <davide.baiocchi97@gmail.com>
2021-05-27 14:51:14 +0200

    sconto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 // Km percorsi
-var kmDaPercorrere = prompt('Km che dovrai percorrere?')
-console.log(kmPercorsi);
+var kmDaPercorrere = prompt ('Km che dovrai percorrere?');
+console.log(kmDaPercorrere);
 
 // Età Cliente
-var eta = prompt('Quanti anni hai?')
+var eta = prompt ('Quanti anni hai?');
 console.log(eta);
+
+// Costo tragitto
+var costoTragitto = kmDaPercorrere * 0.21;
+
+var sconto = 0;
+
+if (eta < 18) {
+    sconto = costoTragitto * 0.2;
+    costoTragitto -= sconto;
+} else if (eta > 65) {
+    sconto = costoTragitto * 0.4;
+    costoTragitto -= sconto;
+} else {
+    costoTragitto -= sconto;
+}
